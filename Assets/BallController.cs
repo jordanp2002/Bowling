ed5039f7c61dab647d00ca0e17a7d0bdbcaf75db
Jordan Pohr <97552850+jordanp2002@users.noreply.github.com>
2025-02-15 11:24:09 -0800

    Camera
    Camerra

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,6 +6,7 @@ public class BallController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    private bool isBallLaunched;
     private Rigidbody ballRB;
     void Start()
     {
@@ -14,12 +15,10 @@ public class BallController : MonoBehaviour
     }
     private void LaunchBall()
     {
+        if (isBallLaunched) return;
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }
